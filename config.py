e2d3d3aa74d7de721b9ecd12732583dd6732e336
Sameer Tantry <sameertantry@gmail.com>
2021-05-04 16:36:45 +0300

    add func1

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ API_TOKEN = environ['API_KEY']
 commands = ['/help - open command list',
             '/news - get latest news and events',
             '/sub - subscribe to the newsletter',
-            '/unsub - cancel subscription']
+            '/unsub - cancel subscription',
+            '/topnews x - get last x[1-8] posts']
 
